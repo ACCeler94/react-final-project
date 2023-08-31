6cb572cc87e63ae767a2b81a711886d080f3d115
Artur C <araghast94@icloud.com>
2023-08-31 21:24:09 +0200

    Connect table form to the state

--- src/components/features/TableForm/TableForm.js
+++ src/components/features/TableForm/TableForm.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './TableForm.module.scss';
 
-const TableForm = () => {
+const TableForm = ({ id, status, peopleAmount, maxPeopleAmount, bill }) => {
+
+  const [tableStatus, setTableStatus] = useState(status);
+  const [peopleCount, setPeopleCount] = useState(peopleAmount ? peopleAmount : '');
+  const [maxPeopleCount, setMaxPeopleCount] = useState(maxPeopleAmount ? maxPeopleAmount : '');
+  const [billAmount, setBillAmount] = useState(bill ? bill : 0);
 
 
   return (
@@ -9,22 +14,22 @@ const TableForm = () => {
       <div className={`${styles.tableStatusWrapper} my-3`}>
         <label htmlFor='table-status' className='fw-bold me-3'>Status:</label>
         <select id='table-status' name='table-status' className={styles.select}>
-          <option value='Free'>Free</option>
-          <option value='Reserved'>Reserved</option>
-          <option value='Busy' >Busy</option>
-          <option value='Cleaning' >Cleaning</option>
+          <option value='Free' selected={tableStatus === 'Free'} >Free</option>
+          <option value='Reserved' selected={tableStatus === 'Reserved'}>Reserved</option>
+          <option value='Busy' selected={tableStatus === 'Busy'} >Busy</option>
+          <option value='Cleaning' selected={tableStatus === 'Cleaning'} >Cleaning</option>
         </select>
       </div>
       <div className={`${styles.peopleRangeWrapper} my-3`}>
         <label id='people-range' htmlFor='people-min' className='fw-bold me-3'>People:</label>
-        <input id='people-min' name='people-min' aria-labelledby='people-range' type='text' />
+        <input id='people-min' name='people-min' aria-labelledby='people-range' type='text' value={peopleCount} />
         <span className={styles.divider}>/</span>
-        <input id='people-max' name='people-max' aria-labelledby='people-range' type='text' />
+        <input id='people-max' name='people-max' aria-labelledby='people-range' type='text' value={maxPeopleCount} />
       </div>
       <div className={`${styles.billWrapper} my-3`}>
         <label htmlFor='bill-amount' className='fw-bold me-4' >Bill:</label>
         $
-        <input id='bill-amount' name='bill-amount' type='text' />
+        <input id='bill-amount' name='bill-amount' type='text' value={billAmount} />
       </div>
       <input type='submit' className='btn btn-primary my-3' value='Update' />
     </form>
